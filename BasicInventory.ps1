956ad674268d42daaaf091db0f4b1162d116b219
luissoares <40070907+luis-soares@users.noreply.github.com>
2019-10-02 13:16:30 -0300

    Update BasicInventory.ps1

--- BasicInventory.ps1
+++ BasicInventory.ps1
@@ -14,14 +14,26 @@ foreach ($computer in $computers.name) {
     #Se tiver comunicação RPC, executa os testes.
     if ($testrpccom -ine $null){
 
-        # CPU
+    Write-host "Hostname: " $computer
+    
+    #Configuracao de IP e Default Gateway 
+    $ips = Get-NetIPAddress |select IPAddress,PrefixLength | Where-Object {$_.IPAddress -notmatch "127.0.0.1" -and $_.IPAddress -notmatch "::1"}
+    $gw = Get-NetRoute | ? {$_.DestinationPrefix -imatch "0.0.0.0"} | select Nexthop
+    write-host "Endereço IP:" $ips
+    write-host "Default Gateway:" $gw 
+
+
+        # CPU 
+        #Media de Utilizacao CPUs
         $cpu = Get-WmiObject -computername $computer win32_processor | Measure-Object -property LoadPercentage -Average | Select Average
+        #Nucleo de CPUs
+        $vcpu = Get-WmiObject -computername $computer win32_processor |select SocketDesignation |Measure-Object | select Count
 
         #Memoria
         $memoria = Get-WmiObject -computername $computer win32_operatingsystem | Select-Object @{Name = "MemoryUsage (%)"; Expression = { "{0:N2}" -f ((($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)*100)/ $_.TotalVisibleMemorySize) }}
         $memoriatotal = Get-WmiObject -computername $computer win32_operatingsystem | Select-Object @{Name = "MemoryTotal(GB)"; Expression = { "{0:N0}" -f (($_.TotalVisibleMemorySize)/1024/1024) }}
 
-        write-host "Nome:" $hostname ";" "Uso de CPU:" $cpu ";" ";" "Memoria Total (GB):" $memoriatotal ";"  "Uso de Memoria:" $memoria ";" "Uso do disco " 
+        write-host "Nome:" $hostname ";" "Nucleos CPU:" $vcpu ";"  "Uso de CPU:" $cpu ";" ";" "Memoria Total (GB):" $memoriatotal ";"  "Uso de Memoria:" $memoria ";" "Uso do disco " 
     
     $discos = Get-WmiObject -ComputerName $computer -Class Win32_logicalDisk 
         foreach ($disco in $discos){
@@ -43,6 +55,3 @@ foreach ($computer in $computers.name) {
 
 
 }
-
-
-    
