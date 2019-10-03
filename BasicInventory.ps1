92dfcd209f696c4656dc303bb63ba6fc1a92220e
luissoares <40070907+luis-soares@users.noreply.github.com>
2019-10-03 15:50:39 -0300

    Update BasicInventory.ps1

--- BasicInventory.ps1
+++ BasicInventory.ps1
@@ -1,7 +1,8 @@
 # Luis Antonio Soares da Silva (lui_eu@msn.com / luissoares@outlook.com)
 # Exit to file. In powershell/cmd exec: powershell -file BasicInventory.ps1 .\log.log > LogInvent.log
 
-$computers = Get-ADComputer -SearchBase "Ou=Servers,DC=Luren,DC=LAB" -filter * |select Name,DistinguishedName
+$computers = Get-ADComputer -SearchBase "Ou=Servers,DC=LUREN,DC=LAB" -filter * |select Name,DistinguishedName
+
 
 function SERIALNUMBER {
 if ($machine -imatch "Virtual"){
@@ -19,18 +20,27 @@ if ($global:hwinfo -imatch "Virtual"){
     $global:machine = "Virtual Machine"
 }
  else
-{$global:machine = "HW:  $global:hwinfo" }
+{
+ $global:machine = $global:hwinfo.model
+ }
 } #VER VIRTUAL/FISICA
 
 function IPINFO {
-        $global:ipsremoto = Invoke-Command -ComputerName $computer -scriptblock { Get-NetIPAddress |select IPAddress,PrefixLength | Where-Object {$_.IPAddress -notmatch "127.0.0.1" -and $_.IPAddress -notmatch "::1" -and $_.IPAddress -notmatch "169.254"} } -ErrorAction SilentlyContinue
-        $global:gwremoto = Invoke-Command -ComputerName $computer -scriptblock {Get-NetRoute | ? {$_.DestinationPrefix -imatch "0.0.0.0"} | select Nexthop } -ErrorAction SilentlyContinue
-        $global:ips = $global:ipsremoto |select  IPAddress,PrefixLength
-        $global:gw = $global:gwremoto |select Nexthop
-        write-host "Endereço IP:" $global:ips
-        write-host "Default Gateway:" $global:gw 
-        Write-host "************"
-
+    if ($global:osinfo -imatch "2003" -or $global:osinfo -imatch "2008") {
+    $global:ips =  Get-WmiObject -ComputerName $computer Win32_NetworkAdapterConfiguration | Where-Object {$_.IPAddress -notmatch "127.0.0.1" -and $_.IPAddress -ine $null -and $_.IPAddress -notmatch "::1" -and $_.IPAddress -notmatch "169.254"} | select IPaddress -ErrorAction SilentlyContinue
+    $global:gw2003 =  Get-WmiObject -ComputerName $computer Win32_NetworkAdapterConfiguration |Where-Object {$_.DefaultIPGateway -ine $null} |select DefaultIPGateway -ErrorAction SilentlyContinue
+    $global:gw = $global:gw2003.DefaultIPGateway
+    }
+    else
+    {
+    $global:ipsremoto = Invoke-Command -ComputerName $computer -scriptblock { Get-NetIPAddress |select IPAddress,PrefixLength | Where-Object {$_.IPAddress -notmatch "127.0.0.1" -and $_.IPAddress -notmatch "::1" -and $_.IPAddress -notmatch "169.254"} } -ErrorAction SilentlyContinue
+    $global:gwremoto = Invoke-Command -ComputerName $computer -scriptblock {Get-NetRoute | ? {$_.DestinationPrefix -imatch "0.0.0.0"} | select Nexthop } -ErrorAction SilentlyContinue
+    $global:ips = $global:ipsremoto |select  IPAddress,PrefixLength
+    $global:gw = $global:gwremoto.Nexthop
+    }        
+    write-host "Endereço IP:" $global:ips.ipaddress
+    write-host "Default Gateway:" $global:gw
+    Write-host "************"
 } #IP AND GW INFOs
 
 function CPUINFO {
@@ -65,6 +75,32 @@ function VALSERVICES {
  $global:services
 } #SERVICES RUNN
 
+function GETOS {
+
+$global:osinfoall = Get-WmiObject -ComputerName $computer win32_operatingsystem
+$global:osinfo = $global:osinfoall.Name.Split('|') -imatch "Microsoft"
+#$global:osarch = $global:osinfoall.OSArchitecture
+$arq = (Get-WmiObject -ComputerName $computer win32_processor).addresswidth 
+    if ($arq -ilike "32"){
+    $global:osarch = "32 Bits"} 
+    else 
+    {$global:osarch = "64 bits"}
+} #VER OPERATION SYSTEM
+
+function GETROLES {
+ Write-host "***************"
+ Write-host "Roles and Features instaladas no servidor " $computer
+ Write-host "***************"
+
+ if ($global:osinfo -imatch "2003" -or $global:osinfo -imatch "2008") {
+ "Funcao de coleta nao disponivel para Windows server 2003 e Windows server 2008"}
+ else
+ {
+ $global:roles = Get-WindowsFeature -ComputerName $computer |? {$_.installstate -imatch "install"}
+ $global:roles |ft Name, InstallState
+ }
+} #VER ROLES AND FEATURES
+
 
 foreach ($computer in $computers.name) {     
     #Criar Variavel
@@ -80,11 +116,14 @@ foreach ($computer in $computers.name) {
        
     MANUFACTURER
     SERIALNUMBER
+    GETOS
 
     Write-host "===================================================================================================="
     Write-host "Hostname: " $computer
-    Write-host "Fabricante: " $global:hwinfo
-    Write-host "Serial: " $global:serialn
+    Write-host "Sistema Operacional: " $global:osinfo
+    Write-Host "Arquitetura: " $global:osarch
+    Write-host "Fabricante: " $global:machine
+    Write-host "Serial: " $global:serialn.SerialNumber
     #Write-host "==================="
 
 
@@ -113,6 +152,8 @@ foreach ($computer in $computers.name) {
     # Serviços em Execucao
     VALSERVICES
 
+    # Roles And Features
+    GETROLES     
     
     Clear-Variable serialn,hwinfo
         
