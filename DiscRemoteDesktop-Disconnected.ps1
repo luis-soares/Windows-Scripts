03668629a6da84199f4818fbd9058e1579444145
luissoares <40070907+luis-soares@users.noreply.github.com>
2019-10-07 22:11:04 -0300

    Update DiscRemoteDesktop-Disconnected.ps1

--- DiscRemoteDesktop-Disconnected.ps1
+++ DiscRemoteDesktop-Disconnected.ps1
@@ -1,30 +1,84 @@
 # Luis Antonio Soares da Silva (lui_eu@msn.com / luissoares@outlook.com)
 
-#SERVERS RDP
-<# $server01 = "rdp01"
-$server02 = "rdp2"
+#SERVERS RDP<
+ $server01 = "LUREN01"
+ $server02 = "LUREN02"
+ $server02 = "LUREN03"
 #>
 
 
 #ALL RDP SERVERS VAR
 $servers = Get-Variable -name server0*,server1*
 
-function loadsess {
-    foreach ($computer in $global:optget){
-    $sessions = query user /server:$computer
-    $global:sessionsact = $sessions |findstr Active
+function mainsess {
+    foreach ($global:computer in $global:optget){
+    $global:sessions = query user /server:$global:computer
+    $global:sessionsact = $sessions |findstr /c:Active /c:Ativo
     $global:sessiondisc = $sessions |findstr Disc
-    }
-} #Get Sess session ID
 
-function discsess {
-foreach ($sessiondiscid in $global:sessiondisc) {
-    $sessiondiscid.Split(" ",[system.stringsplitoptions]::removeemptyentries)[1]
-    write-host "Desconectando a Sessao :" $sessiondiscid
+    switch ($global:optact){
+    val {getsess}
+    dis {discsess}
+    Default {write-host -ForegroundColor Red "Falha, verificar Switch case mainsess"}
     }
+  }
+ } #Get Sess session ID
+
+
+ #DISC MENU
+ function discsessmenu{
+
+Write-host -ForegroundColor DarkYellow "=============================================="
+Write-host -ForegroundColor DarkYellow "          LOGOFF EM SESSOES INATIVAS          "
+Write-host -ForegroundColor DarkYellow "=============================================="
+
+do {
+Write-host -ForegroundColor DarkCyan "O que deseja fazer?"
+
+Write-host -ForegroundColor DarkCyan "
+(1) Desconectar sessoes inativas de todos os servidores;
+(2) Desconectar sessoes inativas de servidor especifico (Ex: RDP01); 
+(S) Voltar ao menu anterior;
+Digite o numero correspondente a opcao e pressione enter:"
+$opt= Read-host 
+
+switch ($opt){
+    1 {$global:optget = $servers.value
+        $global:optact = "dis"
+        mainsess
+      }
+    2 {$global:optget = Read-Host "Informe o(s) nome(s) do(s) servidor(es) (ex: RDP01, RDP2 - >1 incluir , ): "
+        $global:optact = "dis"
+        mainsess
+    }
+    S {Write-host "SAIR"}
+    Default {write-host -ForegroundColor Red "Opcao Invalida"}
+    }
+
+} until ($opt -imatch "s")
+
+} #MENU to Validate: Disconnected Remote Desktop Session
+
+
+#DISC FUNC
+function discsess {
+
+foreach ($sessiondiscid in $global:sessiondisc) {
+    $id = $sessiondiscid.Split(" ",[system.stringsplitoptions]::removeemptyentries)[1]
+        write-host "Desconectando a Sessao" $id "do servidor" $global:computer "... aguarde 5 min e valide, pode ser necessario matar processos travados de usuarios"
+    $sessiondiscid
+    reset session /server:$global:computer $id
+    }
+
+
+
+
+
 } #Disconnected Remote Desktop Session
 
-function getsess{
+
+#GET MENU
+function getsessmenu{
 
 Write-host -ForegroundColor DarkYellow "=============================================="
 Write-host -ForegroundColor DarkYellow "       VERIFICANDO OS STATUS DE CONEXÃO       "
@@ -35,22 +89,19 @@ Write-host -ForegroundColor DarkCyan "O que deseja fazer?"
 
 Write-host -ForegroundColor DarkCyan "
 (1) Validar todos os servidores;
-(2) Digitar o nome do servidor (Ex: RDP01); 
+(2) Validar servidor especifico (Ex: RDP01); 
 (S) Voltar ao menu anterior;
 Digite o numero correspondente a opcao e pressione enter:"
 $opt= Read-host 
 
 switch ($opt){
     1 {$global:optget = $servers.value
-        loadsess
-        $act = $global:sessionsact | Measure-Object
-        $disc =  $global:sessiondisc |Measure-Object
-
-    }
+        $global:optact = "val"
+        mainsess
+      }
     2 {$global:optget = Read-Host "Informe o(s) nome(s) do(s) servidor(es) (ex: RDP01, RDP2 - >1 incluir , ): "
-        loadsess
-        $act = $global:sessionsact | Measure-Object
-        $disc =  $global:sessiondisc |Measure-Object
+        $global:optact = "val"
+        mainsess
     }
     S {Write-host "SAIR"}
     Default {write-host -ForegroundColor Red "Opcao Invalida"}
@@ -58,24 +109,20 @@ switch ($opt){
 
 } until ($opt -imatch "s")
 
-}
-
-function getsess {
-if ($optget -ieq "getallserv"){
-   
-    }
-else 
-    {$computers = Read-Host "Informe o(s) nome(s) do(s) servidor(es) (ex: RDP01, RDP2 - >1 incluir , ): "}
+} #MENU to Validate: Get Session Remote Desktop Session
 
 
-}
-
-
-
-
-foreach ($server in $servers.value) {
-   write-host "host " $server
-}
+#GET FUNC
+function getsess{
+    Write-Host "========================================="
+    Write-Host "        SERVIDOR: "$global:computer
+    Write-Host "========================================="
+    $global:sessions
+    Write-Host "========================================="
+    Write-host -ForegroundColor Green "Total de usuarios conectados no servidor $global:computer : " $global:sessionsact.count
+    Write-host -ForegroundColor Red "Total de usuarios desconectados no servidor $global:computer : " $global:sessiondisc.count
+    Write-Host "========================================="
+} #Get Session Remote Desktop Session (Count)
 
 
 
@@ -92,8 +139,8 @@ $opt= Read-host
 switch ($opt){
     
 
-    1 {versess}
-    2 {discsess}
+    1 {getsessmenu}
+    2 {discsessmenu}
     S {Write-host "SAIR"}
     Default {write-host -ForegroundColor Red "Opcao Invalida"}
 
